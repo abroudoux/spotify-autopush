e9e0cc31b943ff95eeab4eb7ce15f6abdd540564
abroudoux <arthur.broudoux@gmail.com>
2023-12-21 16:31:04 +0100

    v1 script (old api)

--- main.py
+++ main.py
@@ -1,2 +1,49 @@
+import requests
+
+from pprint import pprint
+
+
+SPOTIFY_GET_CURRENT_TRACK_URL = ''
+SPOTIFY_ACCESS_TOKEN = ''
+
+
 def get_current_track(access_token):
-    reponse = requests.get(SPOTIFY_GET_CURRENT_TRACK_URL, headers={"Authorization": f"Bearer {access_token}"})
+    response = requests.get(
+        SPOTIFY_GET_CURRENT_TRACK_URL,
+        headers = {
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+
+    if response.status_code == 200:
+        resp_json = response.json()
+
+        track_id = resp_json['item']['id']
+        track_name = resp_json['item']['name']
+        artists = resp_json['item']['artists']
+        artists_names = ", ".join(
+            [artist['name'] for artist in artists]
+        )
+        link = resp_json['item']['external_urls']['spotify']
+
+        current_track_info = {
+            "id": track_id,
+            "name": track_name,
+            "artists": artists_names,
+            "link": link,
+        }
+        return current_track_info
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def main():
+    current_track_info = get_current_track(
+        SPOTIFY_ACCESS_TOKEN
+    )
+
+    pprint(current_track_info, indent = 4)
+
+
+if __name__ == '__main__':
+    main()
