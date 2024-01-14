7d54437dbceb5d33bc21db556c65497848f7f804
arthur <arthur.broudoux@gmail.com>
2024-01-14 23:42:45 +0100

    get most_recent_track

--- spotify_autopush/main.py
+++ spotify_autopush/main.py
@@ -20,10 +20,18 @@ def app():
     if not redirect_uri:
         print("Veuillez définir la variable d'environnement SPOTIPY_REDIRECT_URI.")
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope="user-library-read"))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope="user-read-recently-played"))
 
-    results = sp.current_user_saved_tracks()
-    for idx, item in enumerate(results['items']):
-        track = item['track']
-        print(idx, track['artists'][0]['name'], " – ", track['name'])
+    results = sp.current_user_recently_played(limit=1)
+
+    if results and 'items' in results and len(results['items']) > 0:
+        most_recent_track = results['items'][0]['track']
+
+        if 'name' in most_recent_track:
+            track_name = most_recent_track['name']
+            print(f"Nom du morceau le plus récent : {track_name}")
+        else:
+            print("La propriété 'name' n'est pas présente dans les données du morceau.")
+    else:
+        print("Aucun morceau récemment joué trouvé.")
 
