eaebe6ed2107dbd2dda9060f57cc348b32e0a4a6
arthur <arthur.broudoux@gmail.com>
2024-02-01 23:08:13 +0100

    add doc

--- spotify_autopush/src/models/github.py
+++ spotify_autopush/src/models/github.py
@@ -6,40 +6,111 @@ import requests
 import os
 
 class Github:
+    """
+    This class is responsible for interacting with the GitHub API.
 
-    def __init__(self) -> None:
+    Attributes:
+    username (str): The GitHub username.
+    repo (str): The GitHub repository name.
+    base_url (str): The GitHub API base URL.
+    base_graphql_url (str): The GitHub GraphQL API base URL.
+    auth (HTTPBasicAuth): The GitHub HTTPBasicAuth object.
+    headers (dict): The GitHub API headers.
+    api_token (str): The GitHub API token.
+
+    Methods:
+    __init__(): The class constructor
+    __get_bio(): Get the GitHub user bio.
+    update_bio(): Update the GitHub user bio.
+    __get_readme(): Get the GitHub repository README.
+    __save_readme(): Save the GitHub repository README.
+    udpate_readme(): Update the GitHub repository README.
+    """
+
+    def __init__(self):
+        """
+        Class constructor.
+
+        Attributes:
+        username (str): The GitHub username.
+        repo (str): The GitHub repository name.
+        base_url (str): The GitHub API base URL.
+        headers (dict): The GitHub API headers.
+        """
+
         load_dotenv()
+
         self.username: str = os.getenv("GITHUB_USERNAME")
         self.repo: str = self.username
+        self.api_token: str = os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
         self.base_url: str = "https://api.github.com"
-        self.base_graphql_url: str = "https://api.github.com/graphql"
-        self.auth: HTTPBasicAuth = HTTPBasicAuth(self.username, os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN"))
-        self.headers = {'Authorization': f'bearer {os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")}', 'Content-Type': 'application/json'}
-        self.api_token = os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
+        self.headers = {'Authorization': f'bearer {self.api_token}', 'Content-Type': 'application/json'}
 
     def __get_bio(self,context):
+        """
+        Get the GitHub user bio.
+
+        Parameters:
+        context (str): The context to be printed before the bio.
+
+        Returns:
+        str: The GitHub user bio.
+        """
+
         r = requests.get('https://api.github.com/user', headers=self.headers)
         json_data = r.json()
         print(context + json_data["bio"])
 
+        return(context + json_data["bio"])
+
     def update_bio(self, new_bio: str):
+        """
+        Update the GitHub user bio.
+
+        Parameters:
+        new_bio (str): The new bio to be set.
+        """
+
         self.__get_bio("Old bio: ")
+
         bio_patch = requests.patch('https://api.github.com/user', json = {'bio': "Last album played : " + new_bio}, headers=self.headers)
         print ("Status:", bio_patch.status_code)
-        self.get_bio("New bio: ")
+
+        self.__get_bio("New bio: ")
 
     def __get_readme(self):
+        """
+        Get the GitHub repository README.
+
+        Returns:
+        str: The GitHub repository README.
+        """
+
         r = requests.get(f'{self.base_url}/repos/{self.username}/{self.repo}/readme', headers=self.headers)
         json_data = r.json()
         self.readme_content = base64.b64decode(json_data["content"]).decode('utf-8')
+
         self.__save_readme()
 
     def __save_readme(self):
+        """
+        Save the GitHub repository README.
+        """
+
         filepath = os.path.join(os.getcwd(), "spotify_autopush", "README.md")
         with open(filepath, "w") as f:
             f.write(self.readme_content)
 
     def udpate_readme(self, last_played_album, album_cover_url):
+        """
+        Update the GitHub repository README.
+        Push the changes to the GitHub repository.
+
+        Parameters:
+        last_played_album (str): The last played album.
+        album_cover_url (str): The album cover URL.
+        """
+
         self.__get_readme()
 
         new_last_album_played = f'<p>{last_played_album}</p>'
--- spotify_autopush/src/models/spotify.py
+++ spotify_autopush/src/models/spotify.py
@@ -4,13 +4,42 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 class Spotify:
+    """
+    A class used to represent the Spotify API.
 
-    def __init__(self) -> None:
+    Attributes:
+    spotify_client_id (str): The Spotify client ID.
+    spotify_client_secret (str): The Spotify client secret.
+    spotify_redirect_uri (str): The Spotify redirect URI.
+
+    Methods:
+    __init__(): The class constructor
+    __get_last_album_played_data(): Get the last album played data.
+    return_last_album_played(): Return the last album played.
+    """
+
+    def __init__(self):
+        """
+        Class constructor.
+
+        Attributes:
+        spotify_client_id (str): The Spotify client ID.
+        spotify_client_secret (str): The Spotify client secret.
+        spotify_redirect_uri (str): The Spotify redirect URI.
+        """
+
         self.spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
         self.spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
         self.spotify_redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
 
     def __get_last_album_played_data(self) -> Dict[str, Any]:
+        """
+        Get the last album played data.
+
+        Returns:
+        Dict[str, Any]: The last album played data.
+        """
+
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=self.spotify_client_id, client_secret=self.spotify_client_secret, redirect_uri=self.spotify_redirect_uri, scope="user-library-read"))
         results = sp.current_user_saved_albums(limit=1)
 
@@ -27,14 +56,19 @@ class Spotify:
         album_cover_art = recently_saved_album['images']
         album_cover_url = album_cover_art[0]['url'] if album_cover_art and len(album_cover_art) > 0 else print("No album cover URL found.")
         album_url = recently_saved_album['external_urls']['spotify'] if 'external_urls' in recently_saved_album else print("No album URL found.")
-
         artist_name = album_artist[0]['name'] if album_artist and len(album_artist) > 0 else print("The property 'name' was not found in the recently played album.")
-
         album_data: Dict[str, Any] = {"album_name": album_name, "artist_name": artist_name, "album_cover_url": album_cover_url, "album_url": album_url}
 
         return album_data
 
     def return_last_album_played(self):
+        """
+        Return the last album played.
+
+        Returns:
+        str: The last album played.
+        """
+
         album_data = self.__get_last_album_played_data()
 
         artist_name = album_data['artist_name']
